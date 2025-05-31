faa23ab72d84dad16aff888f3f1409f55e02d7f4
Keith Chong <70758815+1702538@users.noreply.github.com>
2025-05-31 11:55:13 +0800

    Modular for index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "react-oidc-context";
-
-const cognitoDomain = "https://ap-southeast-1vffrnajfr.auth.ap-southeast-1.amazoncognito.com";
-
-const cognitoAuthConfig = {
-  authority: cognitoDomain,
-  client_id: "407du18cnkp5u5u978gdrpi10a",
-  redirect_uri: "https://main.d25ickgp2g070n.amplifyapp.com",
-  response_type: "code",
-  scope: "openid email profile",
-  metadata: {
-    issuer: cognitoDomain,
-    authorization_endpoint: `${cognitoDomain}/oauth2/authorize`,
-    token_endpoint: `${cognitoDomain}/oauth2/token`,
-    userinfo_endpoint: `${cognitoDomain}/oauth2/userInfo`,
-    end_session_endpoint: `${cognitoDomain}/logout`,
-    jwks_uri: `${cognitoDomain}/.well-known/jwks.json`,
-  },
-  onSigninCallback: () => {
-    window.history.replaceState({}, document.title, window.location.pathname);
-  },
-};
+import cognitoAuthConfig from "./utils/authConfig";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ src/utils/authConfig.js
@@ -0,0 +1,22 @@
+const cognitoDomain = process.env.REACT_APP_COGNITO_DOMAIN;
+
+const cognitoAuthConfig = {
+  authority: cognitoDomain,
+  client_id: process.env.REACT_APP_COGNITO_CLIENT_ID,
+  redirect_uri: process.env.REACT_APP_COGNITO_REDIRECT_URI,
+  response_type: "code",
+  scope: "openid email profile",
+  metadata: {
+    issuer: cognitoDomain,
+    authorization_endpoint: `${cognitoDomain}/oauth2/authorize`,
+    token_endpoint: `${cognitoDomain}/oauth2/token`,
+    userinfo_endpoint: `${cognitoDomain}/oauth2/userInfo`,
+    end_session_endpoint: `${cognitoDomain}/logout`,
+    jwks_uri: `${cognitoDomain}/.well-known/jwks.json`,
+  },
+  onSigninCallback: () => {
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+};
+
+export default cognitoAuthConfig;
